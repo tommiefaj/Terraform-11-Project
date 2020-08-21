d1cf59ae4d98e293c7e6dbadffec3c86e796cc19
Muhammad Rehan <66232567+mxrehan@users.noreply.github.com>
2020-08-21 13:42:03 -0700

    Create S3-bucket.tf

---
+++ S3-bucket.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "foo" {
+  bucket        = "bar"
+  acl           = "public-read-write"
+  force_destroy = true
+  versioning {
+     enabled = false
+  }  
+}
+
+
+
+#   acl           = "public-read-write"
+#   acl           = "private"
+#   Rehan
